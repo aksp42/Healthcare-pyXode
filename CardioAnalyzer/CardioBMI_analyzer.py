b1c3ca3eec3fa9872acbf411b58381aa034eecca
AkaVortex <akanksha24d@gmail.com>
2025-04-25 14:42:02 +0530

    Create CardioBMI_analyzer.py

---
+++ CardioAnalyzer/CardioBMI_analyzer.py
@@ -0,0 +1,1 @@
+This is a beginner-level Python project that calculates BMI and evaluates heart disease risk based on user input factors like age, blood pressure, cholesterol, diabetes, and lifestyle.
